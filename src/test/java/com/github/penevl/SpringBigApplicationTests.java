7b60e2e9a41e57e7737d9d44b0537ddead324c61
elduko <elduko@pop-os.localdomain>
2022-06-17 23:14:45 +0300

    Initial commit

---
+++ src/test/java/com/github/penevl/SpringBigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.penevl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
