e95a09d3614c4abd573ab8c3baaae3b931fa7bc6
penevl <103186686+penevl@users.noreply.github.com>
2022-06-18 17:14:15 +0300

    generated user getters and setters

--- src/main/java/com/github/penevl/user/User.java
+++ src/main/java/com/github/penevl/user/User.java
@@ -16,6 +16,55 @@ public class User {
     String email;
     String password;
     int phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public List<Product> getCart() {
+        return cart;
+    }
+
+    public void setCart(List<Product> cart) {
+        this.cart = cart;
+    }
+
     List<Product> cart = new ArrayList<>();
 
     public User(){}
