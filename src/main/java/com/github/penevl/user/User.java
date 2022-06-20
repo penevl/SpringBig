782101151fb912064b3334982f99976b08afb30d
penevl <103186686+penevl@users.noreply.github.com>
2022-06-20 22:01:21 +0300

    Added a library for making fake accounts

--- src/main/java/com/github/penevl/user/User.java
+++ src/main/java/com/github/penevl/user/User.java
@@ -1,10 +1,6 @@
 package com.github.penevl.user;
 
-import com.github.penevl.product.Product;
-
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Table(name = "users")
 @Entity
@@ -16,7 +12,7 @@ public class User {
     String name;
     String email;
     String password;
-    long phone;
+    String phone;
     public int getId() {
         return id;
     }
@@ -49,17 +45,17 @@ public class User {
         this.password = password;
     }
 
-    public long getPhone() {
+    public String getPhone() {
         return phone;
     }
 
-    public void setPhone(int phone) {
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 
     public User(){}
 
-    public User(String name, String email, String password, long phone) {
+    public User(String name, String email, String password, String phone) {
         this.id = id;
         this.name = name;
         this.email = email;
--- src/main/java/com/github/penevl/user/UserRepository.java
+++ src/main/java/com/github/penevl/user/UserRepository.java
@@ -3,4 +3,5 @@ package com.github.penevl.user;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User,Integer> {
+    public User findByName(String name);
 }
